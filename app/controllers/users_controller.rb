c6228200e313fba816e59139627576093cc02cfc
Mergen Battur <mbattur@gmail.com>
2018-08-22 09:53:24 -0500

    Ongoing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
 
   def my_portfolio
-    @user = current_user
     @user_stocks = current_user.stocks
+    @user = current_user
   end
-
+  
   def my_friends
     @friendships = current_user.friends
   end
-
+  
   def search
     if params[:search_param].blank?
       flash.now[:danger] = "You have entered an empty search string"
     else
-      @users = User.search(params[:stock])
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
       flash.now[:danger] = "No users match this search criteria" if @users.blank?
     end
     respond_to do |format|
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,23 +11,32 @@ class User < ApplicationRecord
 
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
-    "Anonymous" 
+    "Anonymous"
   end
-
+  
+  
   def stock_already_added?(ticker_symbol)
     stock = Stock.find_by_ticker(ticker_symbol)
     return false unless stock
     user_stocks.where(stock_id: stock.id).exists?
   end
-
+  
   def under_stock_limit?
     (user_stocks.count < 10)
   end
-
+  
   def can_add_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_added?(ticker_symbol)
   end
-
+  
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
+  
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1
+  end
+  
   def self.search(param)
     param.strip!
     param.downcase!
@@ -35,20 +44,20 @@ class User < ApplicationRecord
     return nil unless to_send_back
     to_send_back
   end
-
+  
   def self.first_name_matches(param)
     matches('first_name', param)
   end
-
+  
   def self.last_name_matches(param)
     matches('last_name', param)
   end
-
+  
   def self.email_matches(param)
     matches('email', param)
   end
-
+  
   def self.matches(field_name, param)
-    User.where("#{field_name} like?", "%#{param}%")
+    where("#{field_name} like ?", "%#{param}%")
   end
 end
